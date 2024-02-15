eb26b3c7bacb845147d85eaf192e9206d06a66e5
victor <vjbsh@hotmail.com>
2024-02-15 14:03:44 -0600

    added neoconf for nvim

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -18,13 +18,15 @@ pub fn App() -> impl IntoView {
 	view! {
 		<Stylesheet id="leptos" href="/pkg/leptos_tauri.css"/>
 		<button
-		class = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
-		on:click=move |_| {
-			action.dispatch(HelloWorldServer{});
-		}
-		>"Hello world!"</button>
-		{
-			move || vals.get()
-		}
+			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+			on:click=move |_| {
+				action.dispatch(HelloWorldServer {});
+			}
+		>
+
+			"Hello world!"
+		</button>
+
+		{move || vals.get()}
 	}
 }
