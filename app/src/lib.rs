b8e2ea07f6e63d5380fc2843d94b3a2272e9859f
victor <vjbsh@hotmail.com>
2024-12-16 13:50:17 -0600

    format

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -4,29 +4,29 @@ use leptos_meta::{provide_meta_context, Stylesheet};
 
 #[component]
 pub fn App() -> impl IntoView {
-	provide_meta_context();
-	let action = create_server_action::<HelloWorldServer>();
-	let vals = create_rw_signal(String::new());
-	create_effect(move |_| {
-		if let Some(resp) = action.value().get() {
-			match resp {
-				Ok(val) => vals.set(val),
-				Err(err) => vals.set(format!("{err:?}")),
-			}
-		}
-	});
-	view! {
-		<Stylesheet id={"leptos"} href={"/pkg/{{project-name}}.css"}/>
-		<button
-			class={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"}
-			on:click={move |_| {
-				action.dispatch(HelloWorldServer {});
-			}}
-		>
+    provide_meta_context();
+    let action = create_server_action::<HelloWorldServer>();
+    let vals = create_rw_signal(String::new());
+    create_effect(move |_| {
+        if let Some(resp) = action.value().get() {
+            match resp {
+                Ok(val) => vals.set(val),
+                Err(err) => vals.set(format!("{err:?}")),
+            }
+        }
+    });
+    view! {
+        <Stylesheet id={"leptos"} href={"/pkg/{{project-name}}.css"} />
+        <button
+            class={"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"}
+            on:click={move |_| {
+                action.dispatch(HelloWorldServer {});
+            }}
+        >
 
-			"Hello world!"
-		</button>
+            "Hello world!"
+        </button>
 
-		{move || vals.get()}
-	}
+        {move || vals.get()}
+    }
 }
